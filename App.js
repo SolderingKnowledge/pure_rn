0861c3b824ebb9993302a36327ba46159c37e4d7
SolderingKnowledge <imaikol@yahoo.com>
2020-01-04 21:22:06 -0800

    third

--- App.js
+++ App.js
@@ -7,22 +7,8 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
+import { LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
 import Header from "./src/components/Header";
 
 const App = () => {
